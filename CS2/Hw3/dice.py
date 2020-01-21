3ddd043080ac2e6c17cf06d4c42a06e16e3b9d1c
Janat Taerakul <janat.taerakul@gmail.com>
2020-01-22 01:44:16 +0900

    Finished dice.py

--- CS2/Hw3/dice.py
+++ CS2/Hw3/dice.py
@@ -4,25 +4,26 @@ random.seed(123)
 
 def one_dice(nb_simulations):
     """ Simulate throwing a 6-sided dice nb_simulations time
-    Print the percentage of each possible value (1 to 6).
+    Print the percentage of each possible value (2 to 12).
     """
-    counter = [0,0,0,0,0,0]  # shorter version: counter = [0]*6
+    counter = [0,0,0,0,0,0,0,0,0,0,0,0]  # shorter version: counter = [0]*12
 
     for i in range(nb_simulations):
         # Throw a dice and get a random (uniform) result
         x = random.randint(1,6)
-        # Be careful x is in {1,...,6}
+        y = random.randint(1,6)
+        # Be careful x is in {1,...,12}
         # but lists are indexed from 0
-        counter[x-1] += 1
+        counter[x+y-1] += 1
     
     print("Raw values:", counter)
     
-    for i in range(6):
+    for i in range(12):
         # Compute percentage and round with 2 digits after the decimal point
         counter[i] = round( counter[i] * 100 / nb_simulations, 2)    
     
     print("Simulation results:")
-    for i in range(1,7):  # i takes values 1,2,...,6
+    for i in range(2,13):  # i takes values 2,3,...,12
         print(f"The dice value was {i} {counter[i-1]}% of time") # Note the [i-1]
 
 # Run the simulation
