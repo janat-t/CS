fa5c726463afc9070bb63d38df2e2068f9d945ff
Janat <janat.taerakul@gmail.com>
2020-02-10 10:04:52 +0900

    Improve Readability

--- CS2/Project3/simulate.py
+++ CS2/Project3/simulate.py
@@ -2,12 +2,12 @@
 
 
 from maze import genRandMaze, BFSMaze # To access functions defined in the other file
-from tqdm import tqdm
+from tqdm import tqdm                 # To create a beautiful progress bar
 import matplotlib.pyplot as plt       # To plot graphics directly in python
 import numpy as np
 
 # YOU HAVE TO WRITE THIS FUNCTION
-def simulateFixedParameters(n, p, nbSimus):
+def simulateFixedParameters(N, P, nbSimus):
     """ Execute *nbSimus* simulations:
         - Generate a maze with parameters *n* and *p*
         - Check if there is a path from S to G
@@ -15,22 +15,22 @@ def simulateFixedParameters(n, p, nbSimus):
     """
     success_count = 0
     for i in range(nbSimus):
-        maze = genRandMaze(n, p)
-        if BFSMaze(n, maze):
+        maze = genRandMaze(N, P)
+        if BFSMaze(N, maze):
             success_count += 1
 
-    return success_count / nbSimus * 100   # Of course you need to remove this “return 0”
+    return success_count / nbSimus * 100   # Return th rate of success
 
 
 # You do *not* need to modify this function
 # But you can if you want to change something
-def simulate(n, listP, nbSimus):
-    """ Excecute nbSimus simulations for all values of p in listP
-        Return the list of percentage of success for each value of p
+def simulate(N, listP, nbSimus):
+    """ Excecute nbSimus simulations for all values of P in listP
+        Return the list of percentage of success for each value of P
     """
     results = []
-    for p in tqdm(listP):
-        results.append( simulateFixedParameters(n, p, nbSimus) )
+    for P in tqdm(listP):
+        results.append( simulateFixedParameters(N, P, nbSimus) )
     return results
 
 
@@ -40,11 +40,11 @@ if __name__ == "__main__":
     # to plot some nice curves
     
     # You can/should modify the values below
-    finest = 41
-    nbSimus = 10000
-    N = 10
+    finest = 4              # Finest of the graph (1, 2, 3, ...)
+    nbSimus = 10000         # Number of Simulations
+    N = 10                  # Size of the maze
 
-    listP = np.linspace(0.0, 1.0, finest)
+    listP = np.linspace(0.0, 1.0, finest * 10 + 1)
     data = simulate(N, listP, nbSimus)
     
     # Example of curve that you can obtain
