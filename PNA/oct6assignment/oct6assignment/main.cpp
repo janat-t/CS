66247a4faf97feeb31e6c98133df2d5edcf5a353
Janat Taerakul <janat.taerakul@gmail.com>
2020-10-06 17:18:26 +0900

    First class of Programming and Numerical Analysis

---
+++ PNA/oct6assignment/oct6assignment/main.cpp
@@ -0,0 +1,23 @@
+//
+//  main.cpp
+//  oct6assignment
+//
+//  Created by 19B60096 on 2020/10/06.
+//  Copyright © 2020 Janat Taerakul. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    char a = '=';
+    int c = a;
+    char b = '1';
+    int d = b;
+    cout << c << '\n';
+    cout << d << '\n';
+    return 0;
+}
+// c is 61
+// d is 49
---
+++ PNA/oct6simplecode/oct6simplecode/main.cpp
@@ -0,0 +1,24 @@
+//
+//  main.cpp
+//  oct6simplecode
+//
+//  Created by 19B60096 on 2020/10/06.
+//  Copyright © 2020 Janat Taerakul. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    double a = 4;
+    double b = 2;
+    double c;
+    c = a + b;
+    cout << c << "\n";
+    string message;
+    message = "peace on Earth";
+    cout << message << "\n";
+    cin >> a;
+    return 0;
+}
